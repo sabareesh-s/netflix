d71bca6673c933e4fd855f8e0e69b418b1f24870
Sabareesh S <sabareesh1221@gmail.com>
2021-10-25 11:46:33 +0530

    adding api

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { API_KEY } from '../../constants/constants'
+import axios from '../../axios'
 import './Banner.css'
 
 function Banner() {
+    
     return (
         <div className="Banner">
             <div className="content">
-                <h1 className='title'>Movie name</h1>
+                <h1 className='title'>Life of Shine the Great</h1>
                 <div className='banner_buttons'>
                     <button className='button'>Play</button>
                     <button className='button'>My list</button>
                 </div>   
-                <h1 className='description'> In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the </h1> 
+                <h1 className='description'> This is the story of a stupid psychopath who bullies children for a living. The main protagonists in the movie are Shine the theetam and Suresh (A cute plant who drinks water 2 times a week) Based on true incidents.</h1> 
             </div> 
             <div className="fade_bottom"></div>           
         </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY = "d325a440f71e774a57691d65614c03e5";
