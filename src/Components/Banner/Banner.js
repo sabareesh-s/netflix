6259c2125870ddcbd62791b35bf77e5d1947b462
Sabareesh S <sabareesh1221@gmail.com>
2021-11-12 11:29:51 +0530

    host

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -8,7 +8,7 @@ function Banner() {
     useEffect(() => {
        axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
            
-           setMovie(response.data.results[4])
+           setMovie(response.data.results[1])
            
        })
     }, []) 
@@ -17,7 +17,7 @@ function Banner() {
         <div style={{backgroundImage : `url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
         className="Banner">
             <div className="content">
-                <h1 className='title'>{ movie.title ? movie.title : movie.name }</h1>
+                <h1 className='title'>{ movie? movie.title : "" }</h1>
                 <div className='banner_buttons'>
                     <button className='button'>Play</button>
                     <button className='button'>My list</button>
